15948b193000e3bcf19afd45beac3a9f51645c1c
Alexis B. <154525045+JebusAB@users.noreply.github.com>
2025-05-22 14:10:44 +0200

    Commit to branch

--- src/main/java/fsiAdministration/DAO/BDDManager.java
+++ src/main/java/fsiAdministration/DAO/BDDManager.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class BDDManager {
 
-    private static final String URL  = "jdbc:postgresql://localhost:5433/FSI_admin";
+    private static final String URL  = "jdbc:postgresql://localhost:5432/FSI_API";
     private static final String USER = "postgres";
-    private static final String PASS = "080600";
+    private static final String PASS = "JAAJ69420";
 
     /* Connexion unique partagée */
     private static Connection connection;
